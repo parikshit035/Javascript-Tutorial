927bef36a41f2dc2333f88e237c4bf8c9790d89e
parikshit035 <parikshitpatil035@gmail.com>
2026-08-31 15:04:00 +0530

    Done an assignment on var let const

---
+++ script.js
@@ -0,0 +1,32 @@
+// var, let, const - comparison const means value declared cant be change and let means we consider it
+// declaritions and intialize
+
+var a; // declare
+var a = 12; // intialize
+
+// global scope Value can be used in whole code eg: var a = 12;
+// fn scoped can be used in fn 
+// block scope can be used in blocks
+
+// reassignment , redeclration
+
+var b = 12;
+b = 32;
+
+var b;
+
+let c = 12;  
+c = 32;
+
+// Temporal dead zone
+
+let d = 12;
+
+
+console.log(e); 
+
+var e = 12;
+
+// Hoisting var f = undefined; goes on line one before console.log(f )
+
+var f = 12;
